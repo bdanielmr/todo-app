488456941b3b48f7231708d9a08d149a0566f2da
bdanielmr <bdanielmr@gmail.com>
2023-03-15 23:02:33 -0500

    feat: update caardItem

--- src/components/CardItem/cardItem.tsx
+++ src/components/CardItem/cardItem.tsx
@@ -1,182 +1,225 @@
-/* eslint-disable */
-import React, { useState, useRef, useEffect } from 'react';
-import { Card, CardContent, Button, CardActions, Grid } from '@mui/material';
-import { CardCronometro } from '../CardCronometro';
-import { CardMenu } from '../CardMenu';
+/* eslint-disable react/require-default-props */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/function-component-definition */
+import React, { FC, useState, useEffect, useRef } from 'react';
+import { Task } from '../../utils/types';
 
-type CardItemProps = {
-  title: string;
-  description: string;
-  priority: '' | 'bajo' | 'medio' | 'largo';
+interface CardProps {
   id: number;
-  completed: boolean;
-  completedAt: number;
-  onRemove: (id: number) => void;
+  title?: string;
+  description: string;
+  onTitleChange: (newTitle: string) => void;
+  onDescriptionChange: (newDescription: string) => void;
+  initialTime: number;
+  currentTime: number;
+  finishTime: number;
+  onFinishTime: (time: number) => void;
+  onTimerUpdate: (time: number) => void;
+}
+
+const getInitialSelectedOption = (initialTime: number) => {
+  if (initialTime <= 1800) return 'Corto (30min)';
+  if (initialTime <= 2700) return 'Medio (45min)';
+  if (initialTime <= 3600) return 'Largo (45min)';
+  return 'Largo';
 };
 
-export function CardItem({
+const CardItem: FC<CardProps> = ({
+  id,
   title,
   description,
-  priority,
-  id,
-  completed,
-  completedAt,
-  onRemove,
-}: CardItemProps) {
-  // useRef to store a reference to the input fields
-  const inputRefs = useRef<{
-    title: HTMLInputElement | null;
-    description: HTMLTextAreaElement | null;
-  }>({ title: null, description: null });
-
-  // useState to maintain the state of the component
-  const [titleCard, setTitleCard] = useState({
-    title,
-    disableTitle: true,
-    description,
-    disableDescription: true,
-  });
-  const [cronometroType, setCronometroType] = useState<
-    '' | 'bajo' | 'medio' | 'largo'
-  >(
-    (localStorage.getItem(`${id}-selectedValue`) ??
-      '') as CardItemProps['priority'],
+  onTitleChange,
+  onDescriptionChange,
+  initialTime,
+  currentTime,
+  finishTime,
+  onFinishTime,
+  onTimerUpdate,
+}) => {
+  const [currentTitle, setCurrentTitle] = useState(title);
+  const [currentDescription, setCurrentDescription] = useState(description);
+  const [isTitleEditing, setIsTitleEditing] = useState(false);
+  const [isDescriptionEditing, setIsDescriptionEditing] = useState(false);
+  const [timer, setTimer] = useState(finishTime || currentTime || initialTime);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [isTaskCompleted, setIsTaskCompleted] = useState(false);
+  const [completedTime, setCompletedTime] = useState(finishTime);
+  const [selectedOption, setSelectedOption] = useState(
+    getInitialSelectedOption(initialTime),
   );
-  const handleCronometroTypeChange = (
-    type: '' | 'bajo' | 'medio' | 'largo',
-  ) => {
-    setCronometroType(type);
-    console.log('ver type', type);
-  };
-  console.log('VERLO  cronometroType', cronometroType);
-  const [completedTime, setCompletedTime] = useState<number | null>(null);
+  const [menuLocked, setMenuLocked] = useState(initialTime !== 0);
+  const [selectedInitialTime, setSelectedInitialTime] = useState(initialTime);
+  const [hasSelectedOption, setHasSelectedOption] = useState(initialTime !== 0);
 
-  const handleRemoveClick = () => {
-    onRemove(id); // se llama a la función onRemove con el id del elemento
-  };
-
-  // function to update the state of the component
-  const handleCardItemChange = (
-    field: keyof typeof titleCard,
-    value: boolean | string,
-  ) => {
-    setTitleCard({ ...titleCard, [field]: value });
+  const intervalId = useRef<NodeJS.Timeout>();
+  const formatTime = (time: number) => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes
+      .toString()
+      .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   };
 
-  //  event handler functions for the input fields
-  const handleOnChangeTitle = (event: React.FormEvent<HTMLInputElement>) => {
-    setTitleCard({ ...titleCard, title: event.currentTarget.value });
+  const isFinished = finishTime !== 0 || isTaskCompleted;
+
+  const handleOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(e.target.value);
+    localStorage.setItem(`selectedOption-${id}`, e.target.value);
+    const newInitialTime = parseInt(e.target.value, 10) * 60;
+    setTimer(newInitialTime);
+    setSelectedInitialTime(newInitialTime);
+    setIsTimerRunning(false);
+    setMenuLocked(true);
+    setHasSelectedOption(true);
   };
 
-  const handleOnChangeDescription = (
-    event: React.FormEvent<HTMLTextAreaElement>,
-  ) => {
-    setTitleCard({ ...titleCard, description: event.currentTarget.value });
-  };
-  let duration = 0;
-  if (priority === '') {
-    duration = completedAt;
-  }
-  if (priority === 'bajo') {
-    duration = 1800;
-  } else if (priority === 'medio') {
-    duration = 2700;
-  } else if (priority === 'largo') {
-    duration = 3600;
-  }
-
-  // useEffect to focus the input fields when they are enabled
   useEffect(() => {
-    const refs = [inputRefs.current.title, inputRefs.current.description];
-    const disabled = [titleCard.disableTitle, titleCard.disableDescription];
-    refs.forEach((ref, i) => {
-      if (ref !== null && !disabled[i]) {
-        ref.focus();
-      }
-    });
-  }, [titleCard.disableTitle, titleCard.disableDescription]);
+    const storedTimer = localStorage.getItem(`timer-${id}`);
+    if (storedTimer) {
+      setTimer(parseInt(storedTimer, 10));
+    } else {
+      setTimer(initialTime);
+    }
+  }, [id, initialTime]);
 
+  useEffect(() => {
+    localStorage.setItem(`timer-${id}`, timer.toString());
+    onTimerUpdate(timer);
+    setCompletedTime(timer);
+  }, [id, timer]);
+
+  useEffect(() => {
+    if (isFinished) {
+      clearInterval(intervalId.current!);
+      setTimer(completedTime || finishTime);
+    } else if (isTimerRunning && timer > 0) {
+      intervalId.current = setInterval(() => {
+        setTimer(prevTimer => prevTimer - 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId.current!);
+    }
+
+    return () => {
+      clearInterval(intervalId.current!);
+    };
+  }, [
+    isTimerRunning,
+    timer,
+    initialTime,
+    onFinishTime,
+    isFinished,
+    completedTime,
+  ]);
+  useEffect(() => {
+    const storedTitle = localStorage.getItem(`title-${id}`);
+    const storedDescription = localStorage.getItem(`description-${id}`);
+    const storedSelectedOption = localStorage.getItem(`selectedOption-${id}`);
+
+    if (storedTitle) {
+      setCurrentTitle(storedTitle);
+    }
+    if (storedDescription) {
+      setCurrentDescription(storedDescription);
+    }
+    if (storedSelectedOption) {
+      setSelectedOption(storedSelectedOption);
+      setHasSelectedOption(true);
+    }
+  }, [id]);
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <Button variant="text" color="error" onClick={handleRemoveClick}>
-        Eliminar
-      </Button>
-      {/* use a button to make the input field selectable */}
-      <Button
-        onClick={() => handleCardItemChange('disableTitle', false)}
-        variant="text"
-        color="primary"
-      >
+    <div>
+      {isTitleEditing ? (
         <input
-          // store the reference to the input field in inputRefs
-          ref={el => (inputRefs.current.title = el)}
-          // handleCardItemChange to update the state
-          // when the input field is blurred
-          onBlur={() => handleCardItemChange('disableTitle', true)}
-          value={titleCard.title}
-          disabled={titleCard.disableTitle}
-          onChange={handleOnChangeTitle}
-          placeholder="Add a title"
+          type="text"
+          value={currentTitle}
+          disabled={isFinished}
+          onChange={e => setCurrentTitle(e.target.value)}
+          onBlur={() => {
+            onTitleChange(currentTitle ?? '');
+            setIsTitleEditing(false);
+            localStorage.setItem(`title-${id}`, currentTitle ?? '');
+          }}
         />
-      </Button>
-
-      <CardContent>
-        {/* same for the description field */}
-        <Button
-          onClick={() => handleCardItemChange('disableDescription', false)}
-          variant="text"
-          color="primary"
+      ) : (
+        <h3 onClick={() => !isFinished && setIsTitleEditing(true)}>
+          {currentTitle}
+        </h3>
+      )}
+      {isDescriptionEditing ? (
+        <textarea
+          value={currentDescription}
+          disabled={isFinished}
+          onChange={e => setCurrentDescription(e.target.value)}
+          onBlur={() => {
+            onDescriptionChange(currentDescription);
+            setIsDescriptionEditing(false);
+            localStorage.setItem(`description-${id}`, currentDescription);
+          }}
+        />
+      ) : (
+        <p onClick={() => !isFinished && setIsDescriptionEditing(true)}>
+          {currentDescription}
+        </p>
+      )}
+      {isFinished ? (
+        <div className="completed-tag">Completado</div>
+      ) : (
+        <select
+          disabled={isFinished || menuLocked}
+          value={selectedOption}
+          onChange={handleOptionChange}
         >
-          <textarea
-            ref={el => (inputRefs.current.description = el)}
-            onBlur={() => handleCardItemChange('disableDescription', true)}
-            value={titleCard.description}
-            disabled={titleCard.disableDescription}
-            onChange={handleOnChangeDescription}
-            placeholder="Add a description"
-          />
-        </Button>
-      </CardContent>
-      <CardActions>
-        <Grid container spacing={0} columns={16}>
-          <Grid
-            item
-            xs={16}
-            flexDirection="column"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
+          <option value="30">Corto (30 min)</option>
+          <option value="45">Medio (45 min)</option>
+          <option value="60">Largo (60 min)</option>
+        </select>
+      )}
+      <div>
+        {isTimerRunning ? (
+          <button
+            disabled={isFinished}
+            onClick={() => setIsTimerRunning(!isTimerRunning)}
           >
-            <CardMenu id={id} onSelect={handleCronometroTypeChange} />
-            {cronometroType === '' ? (
-              completed && (
-                <CardCronometro
-                  completedTask={completed}
-                  completedAtTask={completedAt}
-                  key={id}
-                  id={id}
-                  type={cronometroType}
-                />
-              )
-            ) : (
-              <CardCronometro
-                completedTask={completed}
-                completedAtTask={completedAt}
-                key={id}
-                id={id}
-                type={cronometroType}
-              />
-            )}
-          </Grid>
-          <Grid
-            item
-            xs={10}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          />
-        </Grid>
-      </CardActions>
-    </Card>
+            Pausar
+          </button>
+        ) : (
+          !isFinished &&
+          (hasSelectedOption || timer !== 0) && ( // Muestra el botón de inicio si se ha seleccionado una opción o si el tiempo es diferente de 00:00:00
+            <button
+              disabled={isFinished}
+              onClick={() => setIsTimerRunning(!isTimerRunning)}
+            >
+              Iniciar
+            </button>
+          )
+        )}
+        {isTimerRunning &&
+          !isFinished &&
+          (hasSelectedOption || timer !== 0) && ( // Muestra el botón de completado si se ha seleccionado una opción o si el tiempo es diferente de 00:00:00
+            <button
+              onClick={() => {
+                setIsTimerRunning(false);
+                setIsTaskCompleted(true);
+                const remainingTime =
+                  selectedInitialTime === 0
+                    ? timer
+                    : selectedInitialTime - timer;
+                setCompletedTime(Math.max(0, remainingTime));
+                onFinishTime(Math.max(0, remainingTime));
+              }}
+            >
+              Completado
+            </button>
+          )}
+      </div>
+      <div>{formatTime(timer)}</div>
+    </div>
   );
-}
+};
+
+export { CardItem };
