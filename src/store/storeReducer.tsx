09e2c30b3ee9dd22cb40106d297a313cae47bdaf
bdanielmr <bdanielmr@gmail.com>
2023-03-15 23:11:36 -0500

    feat: add store and provider context

--- src/store/storeReducer.tsx
+++ src/store/storeReducer.tsx
@@ -1,16 +1,98 @@
+/* eslint-disable max-len */
 import { AppState, ActionState } from '../utils/types';
 
 const types = {
   getDataBoardTask: 'GET-DATA-BOAR-TASK',
+  getDataHome: 'GET-DATA-HOME',
+  createTask: 'CREATE-TASK',
+  updateTask: 'UPDATE_TASK',
 };
 
-const initialStore = {
-  dataBoardTask: [],
+const initialStore: AppState = {
+  dataBoardTask:
+    JSON.parse(localStorage.getItem('dataBoardTask') || '[]').length > 0
+      ? JSON.parse(localStorage.getItem('dataBoardTask') || '[]')
+      : [
+          {
+            id: 1,
+            name: 'task 1',
+            column: 'Done',
+            title: 'new title',
+            description: 'new desxcription',
+            completed: true,
+            type: 'todos',
+            time: {
+              initialTime: 0,
+              currentTime: 120,
+              finishTime: 120,
+            },
+          },
+          {
+            id: 2,
+            name: 'task 2',
+            column: 'Stories',
+            title: 'new title',
+            description: 'new desxcription',
+            completed: false,
+            type: 'todos',
+            time: {
+              initialTime: 1200,
+              currentTime: 0,
+              finishTime: 0,
+            },
+          },
+          {
+            id: 3,
+            name: 'task 3',
+            column: 'Stories',
+            title: 'new title',
+            description: 'new desxcription',
+            completed: false,
+            type: 'todos',
+            time: {
+              initialTime: 600,
+              currentTime: 0,
+              finishTime: 0,
+            },
+          },
+        ],
+  dataHome: {
+    titleHeader: 'Todo App',
+    sidebarItems: [
+      {
+        id: 'Home',
+        label: 'Home',
+        value: 'Home',
+      },
+      {
+        id: 'Task History',
+        label: 'Task History',
+        value: 'Task History',
+      },
+    ],
+  },
 };
 
 const storeReducer = (state: AppState, action: ActionState) => {
   switch (action.type) {
     case types.getDataBoardTask:
+      return {
+        ...state,
+        dataBoardTask: action.payload,
+      };
+    case types.getDataHome:
+      return {
+        ...state,
+        dataHome: action.payload,
+      };
+    case types.updateTask:
+      return {
+        ...state,
+        dataBoardTask: state.dataBoardTask.map(task =>
+          task.id === action.payload.id ? action.payload : task,
+        ),
+      };
+    case types.createTask:
       return {
         ...state,
         dataBoardTask: action.payload,
