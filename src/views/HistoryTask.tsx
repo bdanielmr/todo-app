de1cc369632869eaaf0c9dcf36b74ca337f588af
bdanielmr <bdanielmr@gmail.com>
2023-03-16 00:50:46 -0500

    fix: lint

--- src/views/HistoryTask.tsx
+++ src/views/HistoryTask.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable react/function-component-definition */
-// DoneTasks.tsx
+/* eslint-disable */
 import React, { useState, useEffect, useContext } from 'react';
 
 import { Link } from 'react-router-dom';
